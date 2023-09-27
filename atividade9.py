bce093a23002f7e240111279cff89442979d18ae
Byziel <146034443+Byziel@users.noreply.github.com>
2023-09-27 09:08:54 -0300

    Update atividade9.py

--- atividade9.py
+++ atividade9.py
@@ -1,8 +1,7 @@
-altura=float("digite tua altura")
-sexo=str("digite tua altura(M para masculino, F para feminino)")
+altura=float(input("digite tua altura"))
+sexo=(input("digite tua altura(M para masculino, F para feminino)"))
 if sexo == "F":
-    peso_ideAL=(72,7* altura)-58 
-elif sexo == "m":
-    peso_ideAL=(62,1*altura)-44.7
-
-    print("peso_ideal")
+    peso_ideAL=(72.7* altura)-58
+else:
+    peso_ideAL=(62.1*altura)-44.7
+    print(peso_ideAL)
