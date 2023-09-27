4de1944a3fde907dc81c4ce758cd97d3a604197b
Byziel <146034443+Byziel@users.noreply.github.com>
2023-09-27 08:47:32 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,9 @@
+nome= str(input("digite tu nombre"))
+print("digite teu sexo em F e M")
+sexo= str(input("digite tu sexo"))
+estado_civil= str(input("digite teu estado civil"))
+if sexo  == "F" and estado_civil == "casada":
+    print("tempo de casada")
+    
+
+
---
+++ atividade9.py
@@ -0,0 +1,8 @@
+altura=float("digite tua altura")
+sexo=str("digite tua altura(M para masculino, F para feminino)")
+if sexo == "F":
+    peso_ideAL=(72,7* altura)-58 
+elif sexo == "m":
+    peso_ideAL=(62,1*altura)-44.7
+
+    print("peso_ideal")
