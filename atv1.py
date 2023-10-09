4ddbef77ad2066323597b840bc101314703d0dbb
Byziel <146034443+Byziel@users.noreply.github.com>
2023-10-09 16:04:06 -0300

    Create atv1.py
    Faça uma função que receba como parâmetro um estado e retorno à cidade daquele estado. Ex "CE"

---
+++ atv1.py
@@ -0,0 +1,12 @@
+def estados(estado):
+    if estado == "CE":
+        print("Fortaleza")
+    if estado == "PE":
+        print("Recife")
+    elif estado == "AC"
+        print("Rio Branco")
+    
+    
+estados("PE")
+    
+    
